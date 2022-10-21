f0a3760c0e423aa1827220b91d8394972cf3a108
M-Aloysie <a.murekatet@alustudent.com>
2022-10-21 07:21:00 -0700

    work done

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,2 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for character in str:
-        if ord(character) >= ord('a') and ord(character) <= ord('z'):
-            char = chr(ord(character) - 32)
-        else:
-            char = chararacter
-        print("{:s}".format(char), end="")
-    print('')
+
+
