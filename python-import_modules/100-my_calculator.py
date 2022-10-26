fb44e71b701c948003ea97897c373bcd2145ed4b
M-Aloysie <a.murekatet@alustudent.com>
2022-10-26 06:57:56 -0700

    building my own calculator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,9 +3,9 @@ from sys import argv
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    args = len(argv)
+    number = len(argv)
 
-    if args != 4:
+    if number != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
                                 
