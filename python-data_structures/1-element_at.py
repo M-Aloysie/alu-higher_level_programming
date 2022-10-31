04ce30f32b819277b810bfc02857132275b020e6
M-Aloysie <a.murekatet@alustudent.com>
2022-10-31 09:09:35 -0700

    retrieving an elemt from the list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,4 +3,4 @@
 
 def element_at(my_list, idx):
     for j in my_list:
-        print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+        print(( {:d} is {}".format(idx, element_at(my_list, idx)))
