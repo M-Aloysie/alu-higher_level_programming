d1c47992f61c5be62c26d54799088a666c59cb75
M-Aloysie <a.murekatet@alustudent.com>
2022-10-31 10:40:23 -0700

    replacing an element without modifying the original list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Replacing an element in a list at a specific position without modifying the original list
+# Replacing an element without modifying the original list
 
 def new_in_list(my_list, idx, element):
     copy = my_list.copy()
