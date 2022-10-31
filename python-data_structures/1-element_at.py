58c12c0172ba5e057a6355dc30ee27a545dfd330
M-Aloysie <a.murekatet@alustudent.com>
2022-10-31 09:31:58 -0700

    retrieving an element from the list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-
+#Afunction that retrieves ann element from a list like in C
 
 def element_at(my_list, idx):
-    for j in my_list:
-        print(("{:d}".format(idx, element_at(my_list, idx)))
+    if idx < 0 or idx > len(my_list) -1:
+        return None
+    else:
+        return my_list[idx]
