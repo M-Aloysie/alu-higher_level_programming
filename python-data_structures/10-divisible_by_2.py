b90f1a8a7474a46c22cee3f23ffb34676e93baf3
M-Aloysie <a.murekatet@alustudent.com>
2022-11-01 09:17:17 -0700

    funstion that find all multiplies by two in the list

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Afunction that finds a multiple of two in a list
+
+
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            new_list.append(true)
+        else:
+            new_list.append(false)
+    return new_list
