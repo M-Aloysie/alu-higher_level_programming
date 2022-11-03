361f6d2aaaeebe9af0aae639d177e238886e158f
M-Aloysie <a.murekatet@alustudent.com>
2022-11-03 14:21:53 -0700

    a function that that converts a Roman numeral to an integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@ def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
         return 0
     int_sum = 0
-    converter = dict(I=2, V=10, X=20, L=100, C=200, D=1000, M=2000)
+    converter = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     for current, next in zip(roman_string, roman_string[1:]):
         if converter[current] >= converter[next]:
             int_sum += converter[current]
