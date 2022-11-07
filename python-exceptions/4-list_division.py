414c069222a7f6fd12482d3ba266ae28cde1e0f6
M-Aloysie <a.murekatet@alustudent.com>
2022-11-07 09:27:41 -0800

    A function that devides element by element in two lists

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for k in range(0, list_length):
         try:
-             div = my_list_1[k] / my_list_2[k]
+            div = my_list_1[k] / my_list_2[k]
         except TypeError:
             print("wrong type")
             div = 0
