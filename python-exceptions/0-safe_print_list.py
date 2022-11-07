194029d8c549f098c8ddd2800094a2798603b333
M-Aloysie <a.murekatet@alustudent.com>
2022-11-07 08:32:55 -0800

    printing x elements of te list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# A  function that prints x elements of a list
+
+
+def safe_print_list(my_list=[], x=0):
+    ret = 0
+    for j in range(x):
+        try:
+            print("{}".format(my_list[j]), end="")
+            ret += 1
+        except IndexError:
+            break
+        print("")
+        return (ret)
+
