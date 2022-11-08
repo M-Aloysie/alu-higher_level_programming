f0345aa4c1364d32889dd8267bf2d23bc3576aa4
M-Aloysie <a.murekatet@alustudent.com>
2022-11-08 10:27:47 -0800

    size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-# A class Square that defines a square by:(based on 1-square.py)
-"""
-   define a class 'Square'
-"""
+"""define a class Square."""
 
 
 class square:
-    """
-        square with private instance attribute: 'size'
-    """
+    """Represent a square"""
 
     def __init__(self, size=0):
-        """
+        """Initialize a new Square.
            Args:
                size (int): size of the new square
         """
