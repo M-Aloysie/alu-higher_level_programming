38893344a4c4de15360b9c79e5ff0d3e09bcd688
M-Aloysie <a.murekatet@alustudent.com>
2022-11-17 07:32:40 -0800

    My integer

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -4,17 +4,17 @@
 
 
 class MyInt(int):
-    """invert int operators '==' and '!='.
+    """invert int operators ==and !=.
     """
 
-def __eq__(self, value):
-    """override '==' opeartor with '!='.
-    """
+    def __eq__(self, value):
+        """override == opeartor with !=.
+        """
 
-    return self.real != value
+        return self.real != value
 
-def __ne__(self, value):
-    """override '!=' operator with '=='.
-    """
+    def __ne__(self, value):
+        """override != operator with ==.
+        """
 
-    return self.real == value
+        return self.real == value
