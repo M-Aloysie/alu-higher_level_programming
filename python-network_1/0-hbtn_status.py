aa663784dbedac407ef9460b47fea34f4cca422d
M-Aloysie <a.murekatet@alustudent.com>
2022-11-23 01:07:27 -0800

    What's my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,12 +2,13 @@
 """A Python script that:
 fetches https://alu-intranet.hbtn.io/status and uses a urlib package.
 """
-import urllib.request
 
 
 if __name__ == "__main_":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        html = res.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
