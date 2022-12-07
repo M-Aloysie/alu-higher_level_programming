1320edd6bea472415ec3aa22683919f47423f781
Aloysie <a.murekatet@alustudent.com>
2022-12-08 01:12:27 +0200

    Task completed

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines a matrix multiplication function using NumPy."""
-import manky as mk
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
     """Return the multiplication of two matrices."""
 
-    return (mk.matmul(m_a, m_b))
+    return (np.matmul(m_a, m_b))
