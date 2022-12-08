1e746a63c6132667812031141b8d087deeb0933f
Aloysie <a.murekatet@alustudent.com>
2022-12-09 01:12:23 +0200

    Task Completed

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-"""creates a class city."""
+# A Python file similar to model_state.py named model_city.py
+"""
+   Define 'City(Base)' class
+   import 'sqlalchemy'
+"""
 
-import sqlalchemy
-from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class City(Base):
-    """class city"""
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False)
+    """Represents a city for a MySQL database.
+    Attributes:
+        id (str): The city's id.
+        name (sqlalchemy.Integer): The city's name.
+        state_id (sqlalchemy.String): The city's state id.
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
