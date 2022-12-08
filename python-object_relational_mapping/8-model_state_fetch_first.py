f3da361db550cb6fd1cbca0ba823d123bb4e40a5
M-Aloysie <a.murekatet@alustudent.com>
2022-12-08 14:56:25 -0800

    Task Completed

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 # script that prints the first 'State' object,
 # from the database 'hbtn_0e_6_usa'
-"""
-   import 'sys' & 'MySQLdb'
-"""
+"""import 'sys' & 'MySQLdb'."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
