4ac3fa86b6b3dc778878a3bca0457c4eb4e7b1f0
M-Aloysie <a.murekatet@alustudent.com>
2023-02-02 21:29:42 +0200

    Task Complete

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 exports.esrever = function (list = []) {
-<<<<<<< HEAD
   const newList = [];
   for (let i = list.length; i >= 0; i--) {
     newList.push(list[i]);
@@ -8,12 +7,3 @@ exports.esrever = function (list = []) {
   newList.shift();
   return newList;
 };
-=======
-    const newList = [];
-    for (let i = list.length; i >= 0; i--) {
-      newList.push(list[i]);
-    }
-    newList.shift();
-    return newList;
-  };
->>>>>>> a48422103c23c7425dcf308552028e7d7bf3b8c2
